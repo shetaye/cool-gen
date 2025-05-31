c63420b168cb5b664213aa0f5225827a845e49b0
Joseph Shetaye <joseph@shetaye.me>
2025-05-30 19:09:46 -0700

    Remove diff markers lol

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,8 @@ mod symbol;
 mod tree;
 mod generator;
 mod environment;
-<<<<<<< HEAD
-=======
 mod fill;
 mod emit;
->>>>>>> 49d4407 (Environment + emit)
 
 fn main() {
     println!("Hello, world!");
